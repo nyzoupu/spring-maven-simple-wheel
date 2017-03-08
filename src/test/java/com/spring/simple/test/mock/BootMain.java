7e198b79fc626e9e58f949171f8c83bbad806b8a
wb-zp195009 <wb-zp195009@alipay.com>
2017-03-08 20:39:41 +0800

    test xml

--- src/test/java/com/spring/simple/test/mock/BootMain.java
+++ src/test/java/com/spring/simple/test/mock/BootMain.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.simple.mock.Boss;
+import com.spring.simple.test.tags.User;
 
 public class BootMain {
 
@@ -14,10 +15,26 @@ public class BootMain {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Boss boss = (Boss)context.getBean("boss");
-        boss.indroduce();
-        Boss boss2 = (Boss)context.getBean("boss2");
-        boss2.indroduce();
+        User user = (User) context.getBean("testBean");  
+        System.out.println(user.getUserName()+ ":" + user.getEmail());  
+        
+//        Boss boss = (Boss)context.getBean("boss");
+//        boss.indroduce();
+//        new Thread(new Thread1(context)).start();
+//        Boss boss2 = (Boss)context.getBean("boss2");
+//        boss2.indroduce();
     }
 
+}
+
+class Thread1 implements Runnable{
+    private ApplicationContext context;
+    public Thread1(ApplicationContext context){
+        this.context = context;
+    }
+    @Override
+    public void run() {
+        Boss boss = (Boss)context.getBean("boss");
+        boss.indroduce();
+    }
 }
---
+++ src/test/java/com/spring/simple/test/tags/UserBeanDefinitionParser.java
@@ -0,0 +1,25 @@
+package com.spring.simple.test.tags;  
+  
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;  
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;  
+import org.springframework.util.StringUtils;  
+import org.w3c.dom.Element;  
+  
+public class UserBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {  
+  
+    protected Class getBeanClass(Element element) {  
+        return User.class;  
+    }  
+  
+    protected void doParse(Element element, BeanDefinitionBuilder bean) {  
+        String userName = element.getAttribute("userName");  
+        String email = element.getAttribute("email");  
+  
+        if (StringUtils.hasText(userName)) {  
+            bean.addPropertyValue("userName", userName);  
+        }  
+        if (StringUtils.hasText(email)) {  
+            bean.addPropertyValue("email", email);  
+        }  
+    }  
+}  
